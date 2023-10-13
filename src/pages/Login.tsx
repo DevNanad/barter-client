4fb390c9724a30963089366f0964d8e257d3f68a
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-10-13 23:49:32 +0800

    add Login UI

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,156 @@
+import { z, ZodType } from 'zod'
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import airDeliver from "../assets/air-delivery.png"
+import {ToastContainer, toast} from "react-toastify"
+import { Navbar } from '../components/Navbar';
+import {Helmet} from "react-helmet-async"
+import { login } from '../api/auth';
+import { useAuthStore } from '../hooks/state';
+
+type LoginFormData = {
+    usernameORemail: string;
+    password: string;
+}
 
 function Login() {
+    const [isLoading, setIsLoading] = useState(false)
+    const [showPassword, setShowPassword] = useState(false);
+    const navigate = useNavigate();
+
+    const schema: ZodType<LoginFormData> = z.object({
+        usernameORemail: z.string().min(1, {message: "Username or Email is required"}).max(255),
+        password: z.string().min(1, {message: "Password is required"}).max(255),
+
+    })
+
+    const {register, handleSubmit, formState:{errors}} = useForm<LoginFormData>({resolver: zodResolver(schema)})
+
+    const handlePasswordToggle = () => {
+        setShowPassword(!showPassword);
+    };
+
+    //login function
+    const handleLogin = async (data: LoginFormData) => {
+        setIsLoading(true)
+        try {
+            const res = await login(data.usernameORemail, data.password)
+
+            const responseData = await res.data
+
+            console.log(responseData);
+            
+            
+            setIsLoading(false)
+            
+            if(responseData.user_type === "trader"){
+                //navigate to trader dashboard
+                useAuthStore.setState({ token: JSON.stringify(responseData)})
+                localStorage.setItem("user_id", JSON.stringify(responseData.user_id))
+                navigate('/trader', {replace: true})
+            }else if (responseData.user_type === 'advertiser') {
+                //navigate to advertiser dashboard
+                useAuthStore.setState({ token: JSON.stringify(responseData)})
+                localStorage.setItem('user_id', JSON.stringify(responseData.user_id))
+                navigate('/advertiser', {replace: true})
+            }else {
+                console.log("No role specified");
+            }
+            
+        } catch (err: any) {
+            //console.log(err)
+            if(err.message === "Network Error"){
+              setIsLoading(false)
+            }else{
+              setIsLoading(false)
+              toast.error(err.response.data.error,{
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+              })
+            }  
+        }
+      };
+
   return (
-    <div>Login</div>
+    <div className='bg-[#BEADFA] h-full'>
+        <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+        />
+
+        <Helmet>
+            <title>BandB | Login</title>
+        </Helmet>
+
+        <Navbar>
+            <Link to="/signup" className='p-2'>Signup</Link>
+        </Navbar>
+        
+        <div className="login-container pt-6 gap-5 md:pt-32 h-full w-full grid md:grid-cols-2">
+            <div className="signup-cloud flex justify-center items-center">
+                <img className='h-32 sm:h-48 md:h-96' src={airDeliver} alt="global shopping image" />
+            </div>
+            <div className="signup-form w-full flex justify-center">
+                <form onSubmit={handleSubmit(handleLogin)} className='w-10/12 lg:w-7/12 p-5 bg-uno rounded-xl flex flex-col shadow-2xl'>
+                    <h2 className='text-center text-dos md:py-5 font-extrabold tracking-wider text-2xl'>Log in</h2>
+                    <div className="fullname w-full font-medium">
+                        <label className="text-dos text-xs">Username or Email</label>
+                        <br />
+                            <input
+                            className="px-4 py-2 bg-uno rounded-lg text-gray-900 text-md outline-none border-solid border-[1px] border-gray-400 w-full tracking-wider"
+                            type="text"
+                            {...register("usernameORemail")}
+                            placeholder="Username or email"
+                            required
+                        />
+                        {errors.usernameORemail && <span className="text-red-400 text-center text-sm">{errors.usernameORemail.message}</span>}
+                    </div>
+
+
+
+                    <div className="password w-full font-medium">
+                        <label className="text-dos text-xs">Password</label>
+                        <br />
+                            <input
+                            className="px-4 py-2 bg-uno rounded-lg text-gray-900 text-md outline-none border-solid border-[1px] border-gray-400 w-full tracking-wider"
+                            type={showPassword ? "text" : "password"}
+                            {...register("password")}
+                            placeholder="Password"
+                            required
+                        />
+                        {errors.password && <span className="text-red-400 text-center text-sm">{errors.password.message}</span>}
+                    </div>
+
+                    <div className="checkme py-2 flex gap-3 items-center">
+                      <input className='h-4 w-4 bg-transparent checked:bg-red-500' type="checkbox" onChange={handlePasswordToggle} />
+                      <p className='text-sm font-medium text-dos'>Show Password</p>
+                    </div>
+                    <p className='text-center text-sm font-normal py-2 text-violet-300'>Forgot password?</p>
+
+                    {isLoading
+                        ? <button className='py-3 px-3 mt-2 md:mt-6 bg-dos rounded-md font-bold text-gray-100 uppercase' disabled >Logging in...</button>
+                        : <button className='py-3 px-3 mt-2 md:mt-6 bg-dos rounded-md font-bold text-gray-100 uppercase' type='submit'>Log In</button>
+                    }
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
