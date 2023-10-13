cacc2ad28a1d71acb6463671cff5d3487f642c63
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-10-13 23:53:17 +0800

    add render svg as component and Logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
+import {FaHandHoldingHand} from "react-icons/fa6"
+
 interface UIProps{
     children: React.ReactNode;
-  }
+}
   
 export function Navbar({children}:UIProps): JSX.Element {
     return (
-        <nav className="flex text-white  pt-8 px-5 sm:px-20 justify-between items-center">
-            <h1 className="font-bold text-xl">LOGO</h1>
+        <nav className="flex text-white  pt-8 px-7 sm:px-20 justify-between items-center">
+            <FaHandHoldingHand className="h-12 w-12 drop-shadow-2xl"/>
             <ul className="flex gap-10 font-medium">
             {children}
             </ul>
