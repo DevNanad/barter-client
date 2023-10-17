665494f59dbde983a5606fdb2555e77f7c4d3ca4
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-10-17 19:08:55 +0800

    add get user via username @ useTrader

--- src/hooks/queries/useTrader.ts
+++ src/hooks/queries/useTrader.ts
@@ -2,8 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosPrivate from "../useAxiosPrivate";
 import { toast } from "react-toastify";
 
-//QUERY FOR GETTING SINGLE VOTER
-export const useTrader = (id:string|null) =>{
+//QUERY FOR GETTING SINGLE TRADER (USERNAME)
+export const useTrader = (id:string) =>{
     const axiosPrivate = useAxiosPrivate()
     return useQuery({
         queryKey: ['trader'], 
@@ -52,4 +52,56 @@ export const useTrader = (id:string|null) =>{
           }
         },
     }) 
-  } 
+}
+
+//QUERY FOR GETTING SINGLE TRADER (USERNAME)
+export const useProfile = (username:string|undefined) =>{
+  const axiosPrivate = useAxiosPrivate()
+  return useQuery({
+      queryKey: ['profile'], 
+      queryFn: async () => {
+        try {
+          const response = await axiosPrivate.get(`/user/trader/profile/${username}`)
+          return response.data
+        } catch (error:any) {
+            if (error.message === 'Network Error') {
+                toast.error('Server Unavailable',{
+                  position: "top-center",
+                  autoClose: 3000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "colored",
+                })
+              } else if(error.response.data?.message){
+                toast.error(error.response.data.message,{
+                  position: "top-center",
+                  autoClose: 3000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "colored",
+                })
+              }else {
+                // Handle other errors
+                error.response.data.errors?.map((err:any) => {
+                  toast.error(err.msg,{
+                      position: "top-center",
+                      autoClose: 3000,
+                      hideProgressBar: false,
+                      closeOnClick: true,
+                      pauseOnHover: true,
+                      draggable: true,
+                      progress: undefined,
+                      theme: "colored",
+                  })
+                })
+              } 
+        }
+      },
+  }) 
+} 
