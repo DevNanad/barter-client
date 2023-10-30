692ab80c454ecef65ba815daec4750e5ce0331ba
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-10-30 20:39:53 +0800

    add get single item query @ useItem

--- src/hooks/queries/useItem.ts
+++ src/hooks/queries/useItem.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery } from "@tanstack/react-query"
 import { toast } from "react-toastify"
 import useAxiosPrivate from "../useAxiosPrivate"
 
@@ -53,5 +53,55 @@ export const useItems = () => {
         },
     }) 
   
-  }
+}
+
+//QUERY FOR GETTING A SINGLE ITEM
+export const useItem = () => {
+  const axiosPrivate = useAxiosPrivate()
+  return useMutation({
+      mutationFn:async (id:string) => {
+          const response = await axiosPrivate.get(`/item/${id}`)
+          return response.data
+      },
+      onError: (error:any) => {
+            if (error.message === 'Network Error') {
+              toast.error('Server Unavailable',{
+                  position: "top-center",
+                  autoClose: 3000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "colored",
+              })
+            } else if(error.response.data?.message){
+              toast.error(error.response.data.message,{
+                position: "top-center",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+              })
+            }else {
+              // Handle other errors
+              error.response.data.errors?.map((err:any) => {
+                toast.error(err.msg,{
+                  position: "top-center",
+                  autoClose: 3000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "colored",
+                })
+              })
+            }
+      }
+  })
+}
   
