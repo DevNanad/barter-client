47ef497f88e69825f51d89ef10cd56e31d6495a3
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-11-13 23:02:39 +0800

    change light and dark colors @ TDash and Profile

--- src/pages/trader/Market.tsx
+++ src/pages/trader/Market.tsx
@@ -1,4 +1,3 @@
-import { BiSearch } from "react-icons/bi"
 import { ToastContainer } from "react-toastify"
 import categories from "../../assets/categories.json"
 import { useItem, useItems } from "../../hooks/queries/useItem";
@@ -53,30 +52,22 @@ export default function Market() {
         pauseOnHover
         theme="colored"
       />
-      <nav className="grid grid-cols-9 z-20 bg-[#E7AD99] dark:bg-[#495867] sticky top-0  place-content-center w-full place-items-center py-3 border-b-2 border-violet-300 dark:border-[#262626]">
-        <div className="search-bar flex items-center w-full h-full col-start-3 col-end-7">
-            <input type="search" placeholder="Search in BandB" className=" bg-gray-200/70 dark:bg-zinc-700 px-3 py-3 h-full rounded-l-md  focus:border-indigo-400 outline-none font-regular text-gray-900 dark:text-gray-100 w-full" />
-            <div className="search-icon flex items-center justify-center pl-3 pr-2 rounded-r-md h-full bg-orange-300 dark:bg-orange-900">
-              <BiSearch className="text-white w-7 h-7" />
-            </div>
-        </div>
-      </nav>
-      <h2 className="text-white py-10 text-center text-xl uppercase tracking-widest font-extrabold">Market</h2>
+      <h2 className="text-black dark:text-gray-100 py-10 text-center text-xl uppercase tracking-widest font-extrabold">Market</h2>
       <div className="body px-5 max-h-40 w-full grid grid-cols-12 grid-flow-row">
-        <div className="category-nav bg-[#C18C5D] col-span-3 lg:col-span-2 flex flex-col items-start text-white  gap-1 rounded-lg">
+        <div className="category-nav bg-white dark:bg-[#212225] col-span-3 lg:col-span-2 flex flex-col items-start text-[#00171f] dark:text-gray-200 gap-1 rounded-lg">
           <h3 className="text-center w-full border-violet-400 dark:border-[#262626] uppercase font-bold border-b-[1px] py-4">Categories</h3>
           <hr className="bg bg-gray-500" />
           {categories.map((category) => (
-            <button onClick={() => getCategory(category.category)} key={category.id} className="category py-2 px-5 font-semibold w-full text-left  dark:hover:bg-zinc-700 hover:bg-[#ECC8AF] text-sm rounded-sm hover:text-violet-500">
+            <button onClick={() => getCategory(category.category)} key={category.id} className="category py-2 px-5 font-semibold w-full text-left  dark:hover:bg-zinc-700 hover:bg-gray-100 text-sm rounded-sm hover:text-violet-500">
               {category.category}
             </button>
           ))}
         </div>
         <div className={`items-category relative  grid  gap-3 px-5 pb-5 col-span-9 lg:col-span-10 rounded-xl  ${isItemSliderOpen ? '' : 'overflow-hidden'} ${byCategory?.length === 0 ? 'grid-cols-1' : 'grid-cols-3 lg:grid-cols-4'}`}>
-              <div className={`organizations ml-5 bg-[#C18C5D] rounded-md w-full min-h-full absolute top-0 left-0 z-10 ${ isItemSliderOpen ? 'translate-x-0' : 'translate-x-full'} transition-all duration-500`}>
+              <div className={`organizations ml-5 bg-white dark:bg-[#212225] rounded-md w-full min-h-full absolute top-0 left-0 z-10 ${ isItemSliderOpen ? 'translate-x-0' : 'translate-x-full'} transition-all duration-500`}>
                 <div className="close flex">
                   <button onClick={() => setIsItemSliderOpen(false)} className="p-5 rounded-xl">
-                    <FaAngleLeft className="w-6 h-6 text-gray-400 dark:text-gray-300"/>
+                    <FaAngleLeft className="w-6 h-6 text-gray-900 dark:text-gray-300"/>
                   </button>
                 </div>
                 <div className="item px-3 pb-4">
@@ -86,10 +77,10 @@ export default function Market() {
                       : <img src={gettedItemData?.image} alt={gettedItemData?.name} className="h-[30rem] min-w-full object-contain" />
                     }
                   </div>
-                  <h3 className="text-xl text-white py-3 font-pop font-semibold capitalize">{gettedItemData?.name}</h3>
-                  <h6 className="text text-white opacity-80 font-pop font-semibold capitalize">₱{gettedItemData?.price}</h6>
-                  <h6 className="text-sm text-white opacity-80  capitalize">Listed in {gettedItemData?.location}</h6>
-                  <div className="details text-white">
+                  <h3 className="text-xl text-gray-800 dark:text-gray-100 py-3 font-pop font-semibold capitalize">{gettedItemData?.name}</h3>
+                  <h6 className="text text-gray-800 dark:text-gray-100 opacity-80 font-pop font-semibold capitalize">₱{gettedItemData?.price}</h6>
+                  <h6 className="text-sm text-gray-800 dark:text-gray-100 opacity-80  capitalize">Listed in {gettedItemData?.location}</h6>
+                  <div className="details text-gray-800 dark:text-gray-100">
                     <h2 className="text-center" >Details</h2>
                     <h5 className="condition opacity-90">Condition: {gettedItemData?.condition}</h5>
                     <p className="details indent-10 p-5">{gettedItemData?.details}</p>
@@ -99,7 +90,7 @@ export default function Market() {
 
                       <Avatar className='flex items-center w-full h-full gap-3'>
                         <AvatarImage className="w-8 h-8 object-cover rounded-full" src={gettedItemData?.user.profile} />
-                        <AvatarFallback className="bg-dos w-8 h-8 text-white uppercase">{gettedItemData?.user.fullname.slice(0,2)}</AvatarFallback>
+                        <AvatarFallback className="bg-dos w-8 h-8 text-gray-800 dark:text-gray-100 uppercase">{gettedItemData?.user.fullname.slice(0,2)}</AvatarFallback>
                         <h3 className="capitalize">{gettedItemData?.user.fullname}</h3>
                       </Avatar>
                     </div>
@@ -110,7 +101,7 @@ export default function Market() {
             {byCategory?.length === 0
               ? <div className="no-items place-self-center dark:text-gray-400 uppercase font-semibold text-sm">Items not found</div>
               : byCategory?.map((item:any) => (
-                <div key={item?.id} className="item text-gray-100 bg-[#C18C5D] rounded-md overflow-hidden flex flex-col shadow-lg h-fit pb-3">
+                <div key={item?.id} className="item text-gray-600 dark:text-gray-400 bg-white dark:bg-[#212225] rounded-md overflow-hidden flex flex-col shadow-lg h-fit pb-3">
                   <Carousel slides={item.image} height="h-40" object="object-cover"/>
                   <div className="flex-1 px-3 flex flex-col">
                     <div className=" pt-3 text-white font-extrabold capitalize">₱{item.price}</div>
@@ -119,7 +110,7 @@ export default function Market() {
                       <IoLocationSharp className="w-4 h-4" />
                       <h4>{item.location}</h4>
                     </div>
-                    <button onClick={() => handleItemDetails(item.id)} className="p-2 bg-[#ECC8AF] text-white shadow-md font-extrabold rounded-md mt-2">Details</button>
+                    <button onClick={() => handleItemDetails(item.id)} className="p-2 bg-dos opacity-80 text-white shadow-md font-extrabold rounded-md mt-2">Details</button>
                   </div>
                 </div>
               ))
--- src/pages/trader/Profile.tsx
+++ src/pages/trader/Profile.tsx
@@ -36,7 +36,7 @@ export default function Profile() {
   }
 
   return (
-    <div className="text-white px-5 min-h-screen overflow-y-auto">
+    <div className="text-gray-900 dark:text-white px-5 min-h-screen overflow-y-auto">
       <div className="about grid grid-cols-9 gap-10 pt-10 mx-5 mb-10">
         <Avatar className='flex w-full h-full items-center justify-end col-span-2'>
           <AvatarImage className="w-40 h-40 object-cover rounded-full" src={traderQuery?.data?.trader?.profile} />
@@ -46,7 +46,7 @@ export default function Profile() {
           <div className="flex gap-2 items-center">
             <h1 className="text-xl font-light mr-5">{traderQuery?.data?.trader?.username}</h1>
             {traderQuery?.data?.trader?.id === user_id
-              ? <Link to='/trader/settings' className="btn dark:bg-gray-700 text-sm font-semibold py-2 px-3 rounded-lg cursor-pointer">Edit profile</Link>
+              ? <Link to='/trader/settings' className="btn bg-gray-300 dark:bg-gray-700 text-sm font-semibold py-2 px-3 rounded-lg cursor-pointer">Edit profile</Link>
               : ''
             }
           </div>
@@ -54,19 +54,19 @@ export default function Profile() {
           <p className=" font-medium capitalize text-sm text-center pt-10">{traderQuery?.data?.trader?.bio}</p>
         </div>
       </div>
-      <Tabs defaultValue="items" className="w-full border-t-[1px] dark:border-[#262626] ">
+      <Tabs defaultValue="items" className="w-full border-t-[1px] border-gray-400 dark:border-[#262626] ">
         <TabsList className="flex justify-center gap-10">
-          <TabsTrigger className="font-bold flex rounded-none text-gray-300 items-center justify-center gap-2 py-3 tracking-wider data-[state=active]:text-white data-[state=active]:border-white data-[state=active]:border-t-[1px]" value="items">
+          <TabsTrigger className="font-bold flex rounded-none text-gray-400 items-center justify-center gap-2 py-3 tracking-wider data-[state=active]:text-dos data-[state=active]:border-dos dark:data-[state=active]:border-white dark:data-[state=active]:text-white data-[state=active]:border-t-[1px]" value="items">
             <FaCartShopping className="w-5 h-5" />
             <h3 className="">ITEMS</h3>
           </TabsTrigger>
-          <TabsTrigger className="font-bold flex rounded-none text-gray-300 items-center justify-center gap-2 py-3 tracking-wider data-[state=active]:text-white data-[state=active]:border-white data-[state=active]:border-t-[1px]" value="contact">
+          <TabsTrigger className="font-bold flex rounded-none text-gray-400 items-center justify-center gap-2 py-3 tracking-wider data-[state=active]:text-dos data-[state=active]:border-dos dark:data-[state=active]:border-white dark:data-[state=active]:text-white data-[state=active]:border-t-[1px]" value="contact">
             <HiMail className="w-5 h-5" />
             <h3 className="">CONTACT</h3>
           </TabsTrigger>
         </TabsList>
         <TabsContent value="items" className={`grid grid-cols-3 relative lg:grid-cols-5 gap-5 ${isItemSliderOpen ? '' : 'overflow-hidden'}`}>
-          <div className={`organizations  bg-[#C18C5D] text-white  rounded-md w-full min-h-full absolute top-0 left-0 z-10 ${ isItemSliderOpen ? 'translate-x-0' : 'translate-x-full'} transition-all duration-500`}>
+          <div className={`organizations  bg-white dark:bg-[#212225] text-white  rounded-md w-full min-h-full absolute top-0 left-0 z-10 ${ isItemSliderOpen ? 'translate-x-0' : 'translate-x-full'} transition-all duration-500`}>
                   <div className="close flex">
                     <button onClick={() => setIsItemSliderOpen(false)} className="p-5 rounded-xl">
                       <FaAngleLeft className="w-6 h-6 text-gray-400 dark:text-gray-300"/>
@@ -80,10 +80,10 @@ export default function Profile() {
                           : <img src={item?.image} alt={item?.name} className="h-[30rem] min-w-full object-contain" />
                         }
                       </div>
-                      <h3 className="text-xl dark:text-gray-300 py-3 font-pop font-semibold capitalize">{item?.name}</h3>
-                      <h6 className="text dark:text-gray-300 opacity-80 font-pop font-semibold capitalize">₱{item?.price}</h6>
-                      <h6 className="text-sm dark:text-gray-300 opacity-80  capitalize">Listed in {item?.location}</h6>
-                      <div className="details dark:text-gray-100">
+                      <h3 className="text-xl text-gray-700 dark:text-gray-300 py-3 font-pop font-semibold capitalize">{item?.name}</h3>
+                      <h6 className="text text-gray-700 dark:text-gray-300 opacity-80 font-pop font-semibold capitalize">₱{item?.price}</h6>
+                      <h6 className="text-sm text-gray-700 dark:text-gray-300 opacity-80  capitalize">Listed in {item?.location}</h6>
+                      <div className="details text-gray-700 dark:text-gray-100">
                         <h2 className="text-center" >Details</h2>
                         <h5 className="condition opacity-90">Condition: {item?.condition}</h5>
                         <p className="details indent-10 p-5">{item?.details}</p>
@@ -94,10 +94,10 @@ export default function Profile() {
           {traderQuery?.data?.trader.Products?.length === 0
             ? <div className="">No Item</div>
             : traderQuery?.data?.trader.Products.map((item: any) => (
-              <div key={item?.id} className="item text-gray-100 bg-[#C18C5D]  rounded-md overflow-hidden flex flex-col shadow-lg h-fit pb-3">
+              <div key={item?.id} className="item text-gray-600 dark:text-gray-400  bg-white dark:bg-[#212225] rounded-md overflow-hidden flex flex-col shadow-lg h-fit pb-3">
                   <Carousel slides={item.image} height="h-40" object="object-cover"/>
                   <div className="flex-1 px-3 flex flex-col">
-                    <div className=" pt-3 text-white font-extrabold capitalize">₱{item.price}</div>
+                    <div className=" pt-3 text-gray-600 dark:text-gray-400  font-extrabold capitalize">₱{item.price}</div>
                     <h2 className="font-bold opacity-80 truncate">{item.name}</h2>
                     <div className="text-xs opacity-50 flex font-light capitalize truncate">
                       <IoLocationSharp className="w-4 h-4" />
@@ -105,7 +105,7 @@ export default function Profile() {
                     </div>
                     {traderQuery?.data?.trader.id === user_id
                       ? ''
-                      : <button onClick={() => handleItemDetails(item.id)} className="p-2 bg-indigo-900 font-extrabold rounded-md mt-2">Details</button>
+                      : <button onClick={() => handleItemDetails(item.id)} className="p-2 bg-dos text-white font-extrabold rounded-md mt-2">Details</button>
                     }
                     
                   </div>
